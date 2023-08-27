5b76cd49df98e84e56f4457957442dfc517ff8b3
JorG1990 <jorge  garcia>
2023-08-27 14:57:44 -0500

    add movieDetails.jsx and movieDetails.styled.jsx

---
+++ src/views/movieDetails/MovieDetails.jsx
@@ -0,0 +1,88 @@
+import { Suspense, useEffect, useState } from "react";
+import { Link, Outlet, useLocation, useParams } from "react-router-dom";
+import { getMovieDetails } from "servicesApi/ApiMovies";
+import { ContainerDetails, ContainerInformation, ContainerMovieDetails, GoBack } from "./MovieDetails.stiled";
+
+const MovieDetails = () => {
+  const { movieId } = useParams();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        const data = await getMovieDetails(movieId);
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, [movieId]);
+
+  const getYear = releaseDate => {
+    const date = new Date(releaseDate);
+    return date.getFullYear();
+  };
+
+  const getGenres = arrGenres => {
+    return arrGenres.map(genre => genre.name).join(", ");
+  };
+  const location = useLocation();
+
+  const cameBack = location.state?.from ?? "/";
+  return (
+    <>
+      <GoBack>
+        <Link to={ cameBack } className="go-back-linck">
+        ↶ Go Back
+        </Link>
+      </GoBack>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <>
+          <ContainerMovieDetails>
+            { data.poster_path ? (
+              <img alt={ data.original_title } src={ `https://image.tmdb.org/t/p/w500${data.poster_path}` }
+              />
+            ) : (
+              <p>Sorry</p>
+            )}
+
+            <ContainerDetails>
+              <h1>
+                { data.original_title } ({ getYear(data.release_date) })
+              </h1>
+              <p>
+                User score: { ~~(data.vote_average * 10) }%
+              </p>
+              <h3>Overview</h3>
+              <p>{ data.overview }</p>
+              <h3>Generes</h3>
+              <p>{ getGenres(data.genre) }</p>
+            </ContainerDetails>
+          </ContainerMovieDetails>
+          <ContainerInformation>
+            <ul>
+              <li>
+                <Link to="cast" state={{ from: cameBack }}>
+                  Reviews
+                </Link>
+              </li>
+            </ul>
+          </ContainerInformation>
+          <Suspense fallback={ <div>Loading subpage...</div> }>
+            <Outlet />
+          </Suspense>
+        </>
+      )}
+    </>
+  );
+};
+
+export default MovieDetails;
---
+++ src/views/movieDetails/MovieDetails.stiled.jsx
@@ -0,0 +1,53 @@
+
+import styled from "styled-components";
+
+export const GoBack = styled.div`
+    padding: 10px;
+    font-size: 18px;
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export const ContainerMovieDetails = styled.div`
+    display: flex;
+    align-items: center;
+    padding-bottom: 5px;
+    border-bottom: 1px solid white;
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.2);
+
+    img{
+      width: 15%;
+      padding-top: 10px;
+      padding-left: 5px;
+    }
+`;
+
+
+export const ContainerDetails = styled.div`
+    margin-left: 25px;
+    margin-right: 10px;
+
+    h1{
+      font-size: 30px;
+    }
+
+    h3{
+      font-size: 20px;
+    }
+
+`;
+
+export const ContainerInformation = styled.div`
+
+  a{
+    font-size: 18px;
+  }
+
+  li:not(:last-child) {
+     margin-bottom: 10px;{
+  }
+
+
+`;
